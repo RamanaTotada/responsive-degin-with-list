19ce3c315016e56a3bb46e127d775c8cdc6adf76
RamanaTotada <totadaramana@gmail.com>
2022-11-20 17:22:30 +0000

    responsive with list

---
+++ src/components/TabItem/index.js
@@ -0,0 +1,24 @@
+import './index.css'
+
+const TabItem = props => {
+  const {tabDetails, updatedActiveTabId, isActive} = props
+  const {tabId, displayText} = tabDetails
+
+  const onClickTabItem = () => {
+    updatedActiveTabId(tabId)
+  }
+  const activeTabClassName = isActive ? 'active-tab-button' : ''
+  return (
+    <li className="tab-item-container ">
+      <button
+        type="button"
+        className={`tab-btn ${activeTabClassName}`}
+        onClick={onClickTabItem}
+      >
+        {displayText}
+      </button>
+    </li>
+  )
+}
+
+export default TabItem
